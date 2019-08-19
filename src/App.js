8dde6a4f1d0640b8deccac640adf7a2a46700814
Jimmy Svensson <js@Jimmys-MacBook-Pro.local>
2019-08-19 13:29:08 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Robot</h1>
+        <p>
+         Här kommer det vara en robot-app sen.
+        </p>
+    </div>
+  );
+}
+
+export default App;
